2b208c88d104a67de0e9fa97d03b0ae22121cb22
triniteej <122570197+triniteej@users.noreply.github.com>
2024-12-17 00:41:00 -0500

    Delete Group-11-master directory
    project-backend in the project backend

--- Group-11-master/src/test/java/com/example/flashcardapp/flashcardappbackend/Flashcard/FlashcardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.flashcardapp.flashcardappbackend.Flashcard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
-    // Additional query methods can be defined here if needed
-}
