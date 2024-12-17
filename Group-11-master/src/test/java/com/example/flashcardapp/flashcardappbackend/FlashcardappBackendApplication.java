b3ddb9542b4da0328d7fc1af8a59b575ad1ebc38
triniteej <122570197+triniteej@users.noreply.github.com>
2024-12-17 00:25:37 -0500

    Add new code to main branch

---
+++ Group-11-master/src/test/java/com/example/flashcardapp/flashcardappbackend/FlashcardappBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.flashcardapp.flashcardappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardappBackendApplication.class, args);
+    }
+}
